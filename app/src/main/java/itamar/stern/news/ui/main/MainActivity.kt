80b8ffa84153def7f7706ff1db369fcd681ef3d6
ItamarSter <93194252+ItamarSter@users.noreply.github.com>
2022-02-11 00:12:02 +0200

    color tablayout

--- app/src/main/java/itamar/stern/news/ui/main/MainActivity.kt
+++ app/src/main/java/itamar/stern/news/ui/main/MainActivity.kt
@@ -1,7 +1,7 @@
 package itamar.stern.news.ui.main
 
 
-import android.graphics.Color
+
 import android.os.Bundle
 import com.google.android.material.tabs.TabLayout
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +13,8 @@ import itamar.stern.news.databinding.ActivityMainBinding
 import itamar.stern.news.NewsApplication
 import itamar.stern.news.models.Category
 
+
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
@@ -22,7 +24,26 @@ class MainActivity : AppCompatActivity() {
         //(observe in onCreate)
         val goToLogin = MutableLiveData(false)
     }
-
+    val tabsColors = arrayOf(
+        R.drawable.layer_bg_selected_tab_general,
+        R.drawable.layer_bg_selected_tab_business,
+        R.drawable.layer_bg_selected_tab_entertainment,
+        R.drawable.layer_bg_selected_tab_health,
+        R.drawable.layer_bg_selected_tab_science,
+        R.drawable.layer_bg_selected_tab_sports,
+        R.drawable.layer_bg_selected_tab_technology,
+        R.drawable.layer_bg_selected_tab_favorites
+    )
+    val tabsBacks = arrayOf(
+        R.color.tab_general,
+        R.color.tab_business,
+        R.color.tab_entertainment,
+        R.color.tab_health,
+        R.color.tab_science,
+        R.color.tab_sports,
+        R.color.tab_technology,
+        R.color.tab_favorites
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -45,16 +66,34 @@ class MainActivity : AppCompatActivity() {
         )
         TabLayoutMediator(tabs, viewPager) { tab, position ->
             tab.text = tabsNames[position]
+            tab.view.setBackgroundResource(tabsBacks[position])
         }.attach()
 
+        //Show the page which the user clicked on it in the welcome screen:
         viewPager.currentItem = NewsApplication.whereToGoFromWelcome
 
+        tabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                tab.view.setBackgroundResource(tabsColors[tab.position])
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+                tab.view.setBackgroundResource(tabsBacks[tab.position])
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
         //When logged-out user trying to mark favorites - send him to login:
         goToLogin.observe(this){
             if(it){
+                tabs.getTabAt(7)?.view?.setBackgroundResource(tabsColors[7])
                 viewPager.currentItem = Category.FAVORITES.second
             }
         }
+
+
+
+
     }
 
 
